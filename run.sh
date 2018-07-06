48870cc13f279079e8c4c8026ede1816194ebb11
petrsnd <petrsnd@gmail.com>
2018-07-06 14:48:50 -0600

    run script can mount volumes now

--- run.sh
+++ run.sh
@@ -2,6 +2,36 @@
 
 ScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+print_usage()
+{
+    cat <<EOF
+USAGE: run.sh [-v volume] [-h] [-c command]
+
+  -h  Show help and exit
+  -v  Directory from host to mount into container at /volume
+      This is useful for mounting a directory with certificates and private keys
+  -c  Alternate command to run in the container (often -c bash to get a prompt)
+      Always specify the -c option last
+
+EOF
+    exit 0
+}
+
+while getopts ":v:h" opt; do
+    case $opt in
+    v)
+        Volume=$OPTARG
+        shift; shift;
+        ;;
+    h)
+        print_usage
+        ;;
+    ?)
+        break
+        ;;
+    esac
+done
+
 if test -t 1; then
     YELLOW='\033[1;33m'
     NC='\033[0m'
@@ -16,7 +46,11 @@ if [ ! -z "$(which docker)" ]; then
             "You can specify an alternate startup command using arguments to this script.\n" \
             "The default entrypoint is bash, so use the -c argument.\n" \
             "  e.g. run.sh -c /bin/bash${NC}"
-    docker run -it safeguard-bash "$@"
+    if [ -z "$Volume" ]; then
+        docker run -it safeguard-bash "$@"
+    else
+        docker run -v "$Volume:/volume" -it safeguard-bash "$@"
+    fi
 else
     >&2 echo "You must install docker to use this script"
 fi
