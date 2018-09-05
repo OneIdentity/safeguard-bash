94b29c5e9fcd84cccd20ff13ac47608a90a7c260
petrsnd <petrsnd@gmail.com>
2018-09-05 15:36:43 -0600

    support logging out a token without a file

--- src/disconnect-safeguard.sh
+++ src/disconnect-safeguard.sh
@@ -4,26 +4,25 @@ print_usage()
 {
     cat <<EOF
 USAGE: disconnect-safeguard.sh [-h]
-       disconnect-safeguard.sh [-v version]
+       disconnect-safeguard.sh [-v version] 
+       disconnect-safeguard.sh [-a appliance] [-t accesstoken] [-v version] 
 
   -h  Show help and exit
+  -a  Network address of the appliance
   -v  Web API Version: 2 is default
+  -t  Safeguard access token
 
 This will call the logout service on the appliance and remove the login file
-that has been stored for the current user. If no login file exists this script
-will do nothing.
+that has been stored for the current user. Or, if an appliance and access
+token are supplied the access token will be logged out.
 
 EOF
     exit 0
 }
 
+Appliance=
+AccessToken=
 Version=2
-
-if [ "$1" = "-h" ]; then
-    print_usage
-elif [ "$2" = "-v" ]; then
-    Version=$3
-fi
 
 LoginFile="$HOME/.safeguard_login"
 
@@ -32,7 +31,26 @@ read_from_login_file()
     cat $LoginFile | grep $1 | cut -d \= -f 2
 }
 
-if [[ -r "$LoginFile" && -f "$LoginFile" ]]; then
+while getopts ":t:a:v:h" opt; do
+    case $opt in
+    t)
+        AccessToken=$OPTARG
+        ;;
+    a)
+        Appliance=$OPTARG
+        ;;
+    v)
+        Version=$OPTARG
+        ;;
+    h)
+        print_usage
+        ;;
+    esac
+done
+
+if [ ! -z "$Appliance" -a ! -z "$AccessToken" ]; then
+    >&2 echo "Ignoring any login file and using specified access token."
+elif [[ -r "$LoginFile" && -f "$LoginFile" ]]; then
     Appliance=$(read_from_login_file Appliance)
     Provider=$(read_from_login_file Provider)
     AccessToken=$(read_from_login_file AccessToken)
@@ -40,16 +58,15 @@ if [[ -r "$LoginFile" && -f "$LoginFile" ]]; then
         Cert=$(read_from_login_file Cert)
         PKey=$(read_from_login_file PKey)
     fi
+
+    >&2 echo "Removing current login file."
+    rm -f $LoginFile
 else
     >&2 echo "A login file was not found or is not readable!"
     exit 1
 fi
 
->&2 echo "Removing current login file."
-rm -f $LoginFile
-
 >&2 echo "Calling logout service on appliance."
-ScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 curl -s -S -k -H 'Accept: application/json' -H "Authorization: Bearer $AccessToken" \
      "https://$Appliance/service/core/v$Version/Token/Logout" -d ""
 
