23cf17d08080c15a2e9b0ceb2724ede1da423ef7
petrsnd <petrsnd@gmail.com>
2018-09-10 15:41:37 -0600

    download ca bundle

--- src/get-trusted-ca-bundle.sh
+++ src/get-trusted-ca-bundle.sh
@@ -55,7 +55,23 @@ Response=$("$ScriptDir/invoke-safeguard-method.sh" -a "$Appliance" -t "$AccessTo
 Error=$(echo $Response | jq .Code 2> /dev/null)
 if [ "$Error" = "null" ]; then
     ApplianceId=$(echo "$Response" | jq -r .Identity)
-    echo $ApplianceId
+    ApplianceName=$(echo "$Response" | jq -r .Name)
+    Response=$("$ScriptDir/invoke-safeguard-method.sh" -a "$Appliance" -t "$AccessToken" -v $Version -s core -m GET -U "SslCertificates?filter=Appliances.Id%20eq%20\"$ApplianceId\"" -N)
+    Error=$(echo $Response | jq .Code 2> /dev/null)
+    if [ "$Error" = "null" ]; then
+        SslCert=$(echo $Response | jq -r .[].Base64CertificateData)
+        IssuerCerts=$(echo $Response | jq -r '.[].IssuerCertificates | join("")')
+        OutFile="$ApplianceName.ca-bundle.crt"
+        if [ -z "$IssuerCerts" ]; then
+            >&2 echo "Saving self-signed SSL certificate to $OutFile"
+            echo "$SslCert" >"$OutFile"
+        else
+            >&2 echo "Saving SSL certificate issuers to $OutFile"
+            echo "$IssuerCerts" >"$OutFile"
+        fi
+    else
+        echo $Response | jq .
+    fi
 else
     echo $Response | jq .
 fi
