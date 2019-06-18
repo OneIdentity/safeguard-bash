5afef37f300f16d22641fc4bea2807055c02482c
petrsnd <petrsnd@gmail.com>
2019-06-18 11:53:21 -0600

    updated handler with comments

--- samples/event-handling/a2a-event-handling/a2a-password-event-handler.sh
+++ samples/event-handling/a2a-event-handling/a2a-password-event-handler.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# This is just a very simple a2a password handler script that just prints
+# the new password every time it is called.
+
 if test -t 1; then
     YELLOW='\033[1;33m'
     NC='\033[0m'
 fi
 
+# This read timeout is important to prevent hanging your event pipeline
 read -t 0.5 Pass
 
 echo -e "${YELLOW}$0 received Password:${NC} $Pass"
+
+# The above functionality could easily be replaced to do something more
+# useful with the password such as modify configuration or restart a service.
 
