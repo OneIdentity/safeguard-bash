fb55ad531f88e942158c21dc86cb22225301295d
petrsnd <petrsnd@gmail.com>
2019-10-05 17:06:53 -0600

    better install script

--- install-local.sh
+++ install-local.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-ScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SourceDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TargetDir="$TargetDir"
 
-cp -R $ScriptDir/src/* $HOME/scripts
+# create scripts directory in your home directory if it doesn't exist
+if [ ! -d "$TargetDir" ]; then
+    mkdir -p "$TargetDir"
+fi
+
+# copy all of safeguard-bash into scripts directory
+cp -R $SourceDir/src/* $TargetDir
+
+# add the scripts directory to your path
+if [[ ":$PATH:" != *":$TargetDir:"* ]]; then
+    PATH="${PATH:+"$PATH:"}$TargetDir"
+fi
+
